bd7423d1d575b6456208c3b26e694f55c833687b
danielbalanica00 <danielbalanica.dev@gmail.com>
2021-12-27 12:00:27 +0100

    made javafx projects non-modular

--- client_manager/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.simpolab.client_manager {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                        
-    opens com.simpolab.client_manager to javafx.fxml;
-    exports com.simpolab.client_manager;
-}
