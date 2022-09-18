fd00de794f278bdec69774509c7eda7b4ce744f0
Andrea Borghesi <00borghesiandrea@gmail.com>
2022-09-18 19:26:17 +0200

    elector displays winners

--- client_elector/src/main/java/com/simpolab/client_elector/session/SessionInfoController.java
+++ client_elector/src/main/java/com/simpolab/client_elector/session/SessionInfoController.java
@@ -1,9 +1,13 @@
 package com.simpolab.client_elector.session;
 
+import com.simpolab.client_elector.domain.Option;
 import com.simpolab.client_elector.domain.Session;
+import com.simpolab.client_elector.utils.HttpUtils;
+import com.simpolab.client_elector.utils.JsonUtils;
 import com.simpolab.client_elector.utils.SceneUtils;
 import java.net.URL;
 import java.util.Date;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -23,6 +27,9 @@ public class SessionInfoController implements Initializable {
   @FXML
   private Text lblState;
 
+  @FXML
+  private Text lblWinner;
+
   public static void init(Session initSession) {
     session = initSession;
   }
@@ -37,5 +44,36 @@ public class SessionInfoController implements Initializable {
     lblSessionName.setText(session.getName());
     lblEndsOn.setText("Ends on: " + new Date(session.getEndsOn() * 1000).toString());
     lblState.setText(session.getState().toString());
+
+    // check for errors
+    if (session.getState().equals(Session.State.ENDED)) {
+      // retrieve options
+      String optionsJson = HttpUtils.get("/api/v1/session/" + session.getId() + "/option");
+      List<Option> options = JsonUtils.parseJsonArray(optionsJson, Option.class);
+
+      // set winner text
+      String winnerJson = HttpUtils.get("/api/v1/session/" + session.getId() + "/result/winner");
+      List<Integer> winnerId = JsonUtils.parseJsonArray(winnerJson, Integer.class);
+      Option winnerOption = options
+        .stream()
+        .filter(opt -> opt.getId().equals(winnerId.get(0)))
+        .findFirst()
+        .get();
+
+      String winMsg = "Winner: " + winnerOption.getValue();
+
+      if (winnerId.size() == 2) {
+        // retrieve winner candidate
+        Option winnerSuboption = options
+          .stream()
+          .filter(opt -> opt.getId().equals(winnerId.get(1)))
+          .findFirst()
+          .get();
+        // extend win message
+        winMsg += "\nCandidate: " + winnerSuboption.getValue();
+      }
+
+      lblWinner.setText(winMsg);
+    }
   }
 }
